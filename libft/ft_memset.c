bb44f009e7be1921c584c8b288fa283768ee0d95
schroederjan <schroeder_jan@posteo.de>
2023-05-05 08:45:01 +0200

    added main to test the function manually

--- libft/ft_memset.c
+++ libft/ft_memset.c
@@ -1,3 +1,4 @@
+/*usr/bin/cc -Wall -Wextra -Werror -g "$0" && exec ./a.out "$@"*/
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
@@ -6,7 +7,7 @@
 /*   By: jschroed <jschroed@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/04 19:15:05 by jschroed          #+#    #+#             */
-/*   Updated: 2023/05/04 19:15:06 by jschroed         ###   ########.fr       */
+/*   Updated: 2023/05/05 08:31:11 by jschroed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,3 +22,45 @@ void	*ft_memset(void *b, int c, size_t n)
 		*(char *)b++ = (unsigned char)c;
 	return (ret);
 }
+
+/* #include <stdio.h> */
+/* #include <stdlib.h> */
+// // simple main to test
+/* int	main(void) */
+/* { */
+/*     char	buf[4]; // allocate enough for the size and the \0 */
+/*     int		ic; */
+/*     size_t	stn; */
+/*  */
+/*     ic = 'a'; // set a char to for the memory to be flushed with */
+/*     stn = 3; // set the size of the flush */
+/*  */
+/*     ft_memset(buf, ic, stn); // flush the memory */
+/*     buf[stn] = 0; // need to add \0 at the end because we use %s to print */
+/*  */
+/*     printf("%s\n", buf);  */
+/* } */
+
+// // more test for using malloc and not needing to add a \0 for printing
+/* int	main(void) */
+/* { */
+/*     char	*buf; */
+/*     int		ic; */
+/*     size_t	stn; */
+/*  */
+/*     ic = 'a'; // set a char to for the memory to be flushed with */
+/*     stn = 3; // set the size of the flush */
+/*      */
+/*     buf = (char *)malloc(stn); */
+/*     if (buf == NULL) */
+/*     { */
+/*         return 1; */
+/*     } */
+/*  */
+/*     ft_memset(buf, ic, stn); // flush the memory */
+/*     for (size_t i = 0; i < stn; i++) */
+/*     { */
+/*         printf("%c", buf[i]); */
+/*     } */
+/*     printf("\n");  */
+/* } */
