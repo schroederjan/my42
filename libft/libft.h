b88915d75cee325d6406b72c76746704eab56d7d
schroederjan <schroeder_jan@posteo.de>
2023-05-07 16:26:45 +0200

    added two new vunctions

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: jschroed <jschroed@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/02 18:38:27 by jschroed          #+#    #+#             */
-/*   Updated: 2023/05/05 20:48:05 by jschroed         ###   ########.fr       */
+/*   Updated: 2023/05/07 14:20:30 by jschroed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,17 +33,18 @@ int		ft_isprint(int c);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 void	*ft_memset(void *b, int c, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
+void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t n);
+// void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
+// int		ft_memcmp(const void *s1, const void *s2, size_t n);
+
 // size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 // int		ft_tolower(int c);
 
 // void	ft_bzero(void *s, size_t n);
-// void	*ft_memcpy(void *dst, const void *src, size_t n);
-// void	*ft_memccpy(void *dst, const void *src, int c, size_t n);
-// void	*ft_memmove(void *dst, const void *src, size_t n);
 // char	*ft_strchr(const char *s, int c);
 // char	*ft_strrchr(const char *s, int c);
 // int		ft_strncmp(const char *s1, const char *s2, size_t n);
-// int		ft_memcmp(const void *s1, const void *s2, size_t n);
 // char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 // void	*ft_calloc(size_t count, size_t size);
 // char	*ft_strdup(const char *s1);
