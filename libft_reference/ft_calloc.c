7f4c5bace619121cc3ae74441ade801a802006a1
schroederjan <schroeder_jan@posteo.de>
2023-05-09 17:01:31 +0200

    added references

---
+++ libft_reference/ft_calloc.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include "libft.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	void	*ret;
+
+	ret = malloc(size * count);
+	if (!ret)
+		return (0);
+	ft_bzero(ret, size * count);
+	return (ret);
+}
