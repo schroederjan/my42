ed72efcb2c6828da1c0814cafff92c4bedc8b7f6
schroederjan <schroeder_jan@posteo.de>
2023-07-05 22:07:26 +0200

    added a function

--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -41,12 +41,45 @@ char	*ft_strchr(char *s, int c)
 	return (0);
 }
 
-/* [> Temp <] */
-/* #include <stdio.h> */
-/* int	main(void) */
-/* { */
-/*     printf("%zu", ft_strlen("abc")); */
-/*     printf("\n"); */
-/*     printf("%c", *ft_strchr("abc", 'b')); */
-/*     printf("\n"); */
-/* } */
+char	*ft_strjoin(char *left_str, char *buff)
+{
+	size_t	i;
+	size_t	j;
+	char *str;
+
+	if (!left_str)
+	{
+		left_str=(char *)malloc(1 * sizeof(char));
+		left_str[0] = '\0';
+	}
+	if (!left_str || !buff)
+		return (NULL);
+	str = malloc(sizeof(char) * ((ft_strlen(left_str) + ft_strlen(buff)) + 1));
+	if (str == NULL)
+		return (NULL);
+	i = -1;
+	j = 0;
+	if (left_str)
+		while(left_str[++i] != '\0')
+			str[i] = left_str[i];
+	while (buff[j] != '\0')
+		str[i++] = buff[j++];
+	str[ft_strlen(left_str) + ft_strlen(buff)] = '\0';
+	free(left_str);
+	return (str);
+}
+
+/* Temp */
+#include <stdio.h>
+#include <string.h>
+int	main(void)
+{
+	printf("%zu", ft_strlen("abc"));
+	printf("\n");
+	printf("%c", *ft_strchr("abc", 'b'));
+	printf("\n");
+	char *left_str = strdup("abc");
+	char *buff = "def";
+	char *result = ft_strjoin(left_str, buff);
+	printf("%s", result);
+}
