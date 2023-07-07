9f2184c138f0c70698d23c2a6c791ad9d2e434ff
schroederjan <schroeder_jan@posteo.de>
2023-07-07 17:49:31 +0200

    finished the functions, cleanup references

--- get_next_line/get_next_line.c
+++ get_next_line/get_next_line.c
@@ -6,10 +6,68 @@
 /*   By: jschroed <jschroed@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/04 20:45:23 by jschroed          #+#    #+#             */
-/*   Updated: 2023/07/04 20:45:42 by jschroed         ###   ########.fr       */
+/*   Updated: 2023/07/06 19:32:46 by jschroed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <unistd.h>
 
+char	*ft_read_to_left_str(int fd, char *left_str)
+{
+	char	*buff;
+	int		rd_bytes;
 
+	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (!buff)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
+	{
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
+		{
+			free(buff);
+			return(NULL);
+		}
+		buff[rd_bytes] = '\0';
+		left_str = ft_strjoin(left_str, buff);
+	}
+	free(buff);
+	return(left_str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*left_str;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0); // not NULL?
+	left_str = ft_read_to_left_str(fd, left_str);
+	if (!left_str)
+		return (NULL);
+	line = ft_get_line(left_str);
+	left_str = ft_get_more(left_str);
+	return (line);
+}
+
+/* [> Temp <] */
+/* #include <stdio.h> */
+/* #include <fcntl.h> */
+/* int	main(void) */
+/* { */
+/*     char *line; */
+/*     int i; */
+/*     int fd; */
+/*  */
+/*     fd = open("test.txt", O_RDONLY); */
+/*     i = 1; */
+/*     while ( i < 7) */
+/*     { */
+/*         line = get_next_line(fd); */
+/*         printf("line [%02d]: %s",i, line); */
+/*         free(line); */
+/*     } */
+/*     close(fd); */
+/* } */
--- get_next_line/get_next_line_utils.c
+++ get_next_line/get_next_line_utils.c
@@ -69,17 +69,74 @@ char	*ft_strjoin(char *left_str, char *buff)
 	return (str);
 }
 
-/* Temp */
-#include <stdio.h>
-#include <string.h>
-int	main(void)
+char	*ft_get_line(char *left_str)
 {
-	printf("%zu", ft_strlen("abc"));
-	printf("\n");
-	printf("%c", *ft_strchr("abc", 'b'));
-	printf("\n");
-	char *left_str = strdup("abc");
-	char *buff = "def";
-	char *result = ft_strjoin(left_str, buff);
-	printf("%s", result);
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (!left_str[i])
+		return (NULL);
+	while (left_str[i] && left_str[i] != '\n')
+		i++;
+	str = (char *)malloc(sizeof(char) * (i + 2));
+	if (!str)
+		return (NULL);
+	i = 0;
+	while (left_str[i] && left_str[i] != '\n')
+	{
+		str[i] = left_str[i];
+		i++;
+	}
+	if (left_str[i] == '\n')
+	{
+		str[i] = left_str[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
+
+char	*ft_get_rest(char *left_str)
+{
+	int		i;
+	int		j;
+	char	*str;
+
+	i = 0;
+	while (left_str[i] && left_str[i] != '\n')
+		i++;
+	if (!left_str[i])
+	{
+		free(left_str);
+		return(NULL);
+	}
+	str = (char *)malloc(sizeof(char) * (ft_strlen(left_str) - i + 1));
+	if (!str)
+		return (NULL);
+	i++;
+	j = 0;
+	while (left_str[i])
+		str[j++] = left_str[i++];
+	str[j] = '\0';
+	free(left_str);
+	return (str);
+}
+
+// TODO make test for main ft_get_more
+// TODO make test for main ft_get_line
+
+/* [> Temp <] */
+/* #include <stdio.h> */
+/* #include <string.h> */
+/* int	main(void) */
+/* { */
+/*     printf("%zu", ft_strlen("abc")); */
+/*     printf("\n"); */
+/*     printf("%c", *ft_strchr("abc", 'b')); */
+/*     printf("\n"); */
+/*     char *left_str = strdup("abc"); */
+/*     char *buff = "def"; */
+/*     char *result = ft_strjoin(left_str, buff); */
+/*     printf("%s", result); */
+/* } */
