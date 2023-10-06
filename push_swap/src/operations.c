30a31cef8ccd725bd00a76faa55c4659064664cc
jschroed <jschroed@c4b8c1.42berlin.de>
2023-10-06 21:27:53 +0200

    added some more functions

--- push_swap/src/operations.c
+++ push_swap/src/operations.c
@@ -6,7 +6,7 @@
 /*   By: jschroed <jschroed@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 19:42:26 by jschroed          #+#    #+#             */
-/*   Updated: 2023/09/19 19:48:36 by jschroed         ###   ########.fr       */
+/*   Updated: 2023/10/06 21:26:59 by jschroed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,3 +41,29 @@ int	sa(t_list **stack_a)
 	ft_putendl_fd("sa", 1);
 	return (0);
 }
+
+// TODO: check this learn this
+int	rotate(t_list **stack)
+{
+	t_list	*head;
+	t_list	*tail;
+
+	if (ft_lstsize(*stack) < 2)
+		return (-1);
+	head = *stack;
+	tail = ft_lstlast(head);
+	*stack = head->next;
+	head->next = NULL;
+	tail->next = head;
+	return (0);
+}
+
+int	ra(t_list **stack_a)
+{
+	if (rotate(stack_a) == -1)
+		return (-1);
+	ft_putendl_fd("ra", 1);
+	return (0);
+}
+
+//TODO: implement rrb
--- push_swap/src/simple_sort.c
+++ push_swap/src/simple_sort.c
@@ -6,13 +6,49 @@
 /*   By: jschroed <jschroed@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 19:28:31 by jschroed          #+#    #+#             */
-/*   Updated: 2023/09/19 19:51:54 by jschroed         ###   ########.fr       */
+/*   Updated: 2023/10/06 21:13:52 by jschroed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
 /* TODO: Implement sort functions above */
+
+static int	get_min(t_list **stack, int val)
+{
+	t_list *head;
+	int		min;
+
+	head = *stack;
+	min = head->index;
+	while (head->next)
+	{
+		head = head->next;
+		if ((head->index < min) && head->index != val)
+			min = head->index;
+	}
+	return (min);
+}
+
+static void	sort_3(t_list **stack_a)
+{
+	t_list	*head;
+	int		min;
+	int		next_min;
+
+	head = *stack_a;
+	min = get_min(stack_a, -1);
+	next_min = get_min(stack_a, min);
+	if (is_sorted(stack_a))
+		return ;
+	if (head->index == min && head->next->index != next_min)
+	{
+		ra(stack_a);
+		sa(stack_a);
+		rra(stack_a);
+	}
+	// TODO: continue
+}
 
 void	simple_sort(t_list **stack_a, t_list **stack_b)
 {
@@ -25,7 +61,7 @@ void	simple_sort(t_list **stack_a, t_list **stack_b)
 	size = ft_lstsize(*stack_a);
 	if (size == 2)
 		sa(stack_a);
-	/* else if (size == 3) */
+	else if (size == 3)
 	/*     s3; */
 	/* else if (size == 4) */
 	/*     s4; */
