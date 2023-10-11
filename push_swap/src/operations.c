34945f4e4e842bcef11cbbed86ebbdf0858e5236
schroederjan <schroeder_jan@posteo.de>
2023-10-11 17:23:58 +0200

    updated ops

--- push_swap/src/operations.c
+++ push_swap/src/operations.c
@@ -6,7 +6,7 @@
 /*   By: jschroed <jschroed@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 19:42:26 by jschroed          #+#    #+#             */
-/*   Updated: 2023/10/06 21:26:59 by jschroed         ###   ########.fr       */
+/*   Updated: 2023/10/09 22:11:05 by jschroed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,7 +42,6 @@ int	sa(t_list **stack_a)
 	return (0);
 }
 
-// TODO: check this learn this
 int	rotate(t_list **stack)
 {
 	t_list	*head;
@@ -66,4 +65,42 @@ int	ra(t_list **stack_a)
 	return (0);
 }
 
-//TODO: implement rrb
+int	rb(t_list **stack_b)
+{
+	if (rotate(stack_b) == -1)
+		return (-1);
+	ft_putendl_fd("rb", 1);
+	return (0);
+}
+
+int	reverse_rotate(t_list **stack)
+{
+	t_list	*head;
+	t_list	*tail;
+
+	if (ft_lstsize(*stack) < 2)
+		return (-1);
+	head = *stack;
+	tail = ft_lstlast(head);
+	while (head)
+	{
+		if (head->next->next == NULL)
+		{
+			head->next = NULL;
+			break ;
+		}
+		head = head->next;
+	}
+	tail->next = *stack;
+	*stack = tail;
+	return (0);
+}
+
+int	 rra(t_list **stack_a)
+{
+	if (reverse_rotate(stack_b) == -1)
+		return (-1);
+	ft_putendl_fd("rrb");
+	return (0);
+}
+
