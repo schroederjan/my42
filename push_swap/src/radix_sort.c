9bd2728aede87a403aa9dde318d01d51769501f2
jan.schroeder <jan.schroeder@dussmann.de>
2023-10-30 09:45:43 +0100

    removed not needed references, updated radix

--- push_swap/src/radix_sort.c
+++ push_swap/src/radix_sort.c
@@ -6,63 +6,57 @@
 /*   By: jschroed <jschroed@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/29 21:41:34 by jschroed          #+#    #+#             */
-/*   Updated: 2023/10/29 21:42:10 by jschroed         ###   ########.fr       */
+/*   Updated: 2023/10/30 09:45:25 by jschroed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/push_swap.h"
 
-// Helper function to get the bit at a specific position
-static int get_bit_at_position(int number, int position) {
-    return (number >> position) & 1;
+static int	get_max_bits(t_list **stack)
+{
+	t_list	*head;
+	int		max;
+	int		max_bits;
+
+	head = *stack;
+	max = head->index;
+	max_bits = 0;
+	while (head)
+	{
+		if (head->index > max)
+			max = head->index;
+		head = head->next;
+	}
+	while ((max >> max_bits) != 0)
+		max_bits++;
+	return (max_bits);
 }
 
-// Find the maximum number in stack_a
-static int find_max(t_list *stack) {
-    int max_val = INT_MIN;
+void	radix_sort(t_list **stack_a, t_list **stack_b)
+{
+	t_list	*head_a;
+	int		i;
+	int		j;
+	int		size;
+	int		max_bits;
 
-    while (stack) {
-        if (stack->value > max_val) {
-            max_val = stack->value;
-        }
-        stack = stack->next;
-    }
-
-    return max_val;
+	i = 0;
+	head_a = *stack_a;
+	size = ft_lstsize(head_a);
+	max_bits = get_max_bits(stack_a);
+	while (i < max_bits)
+	{
+		j = 0;
+		while (j++ < size)
+		{
+			head_a = *stack_a;
+			if (((head_a->index >> i) & 1) == 1)
+				ra(stack_a);
+			else
+				pb(stack_a, stack_b);
+		}
+		while (ft_lstsize(*stack_b) != 0)
+			pa(stack_a, stack_b);
+		i++;
+	}
 }
-
-// Calculate the number of bits required to represent a number
-static int calculate_bits(int number) {
-    int bits = 0;
-
-    while (number) {
-        bits++;
-        number >>= 1;
-    }
-
-    return bits;
-}
-
-void radix_sort(t_list **stack_a, t_list **stack_b) {
-    int max_num = find_max(*stack_a);
-    int max_bits = calculate_bits(max_num);
-
-    for (int position = 0; position < max_bits; position++) {
-        int size = ft_lstsize(*stack_a);
-
-        for (int i = 0; i < size; i++) {
-            if (get_bit_at_position((*stack_a)->value, position) == 0) {
-                pb(stack_a, stack_b);  // Push to stack_b
-            } else {
-                ra(stack_a);  // Rotate stack_a
-            }
-        }
-
-        // Move numbers back from stack_b to stack_a
-        size = ft_lstsize(*stack_b);
-        for (int i = 0; i < size; i++) {
-            pa(stack_a, stack_b);  // Push to stack_a
-        }
-    }
-}
-
