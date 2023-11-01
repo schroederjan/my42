8e58b1f7ca11cc662e1f70d18782a67b47b83332
jan.schroeder <jan.schroeder@dussmann.de>
2023-11-01 11:22:47 +0100

    fix check

--- push_swap/check.sh
+++ push_swap/check.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-ARG="1 2 3 4 3" bash -c './push_swap $ARG | ./checker_linux $ARG'
+ARG="1 2 3 22 888 78 81 10 12222 99 11 23" bash -c './push_swap $ARG | ./checker_linux $ARG'
