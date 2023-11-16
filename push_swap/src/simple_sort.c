4e5826a09514b61fe64a4fae8b13e62c8726c68d
jan.schroeder <jan.schroeder@dussmann.de>
2023-11-16 10:41:21 +0100

    cleanup and more comments

--- push_swap/src/simple_sort.c
+++ push_swap/src/simple_sort.c
@@ -6,7 +6,7 @@
 /*   By: jschroed <jschroed@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 19:28:31 by jschroed          #+#    #+#             */
-/*   Updated: 2023/11/01 09:45:28 by jschroed         ###   ########.fr       */
+/*   Updated: 2023/11/16 09:32:14 by jschroed         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -74,43 +74,6 @@ static void	sort_3(t_list **stack_a)
 	else
 		rra(stack_a);
 }
-
-// TODO: keep this first
-/* static void	sort_3(t_list **stack_a) */
-/* { */
-/*     t_list	*head; */
-/*     int		min; */
-/*     int		next_min; */
-/*  */
-/*     head = *stack_a; */
-/*     min = get_min(stack_a, -1); */
-/*     next_min = get_min(stack_a, min); */
-/*     if (is_sorted(stack_a)) */
-/*         return ; */
-/*     if (head->index == min && head->next->index != next_min) */
-/*     { */
-/*         ra(stack_a); */
-/*         sa(stack_a); */
-/*         rra(stack_a); */
-/*     } */
-/*     else if (head->index == next_min) */
-/*     { */
-/*         if (head->next->index == min) */
-/*             sa(stack_a); */
-/*         else */
-/*             rra(stack_a); */
-/*     } */
-/*     else */
-/*     { */
-/*         if (head->next->index == min) */
-/*             ra(stack_a); */
-/*         else */
-/*         { */
-/*             sa(stack_a); */
-/*             rra(stack_a); */
-/*         } */
-/*     } */
-/* } */
 
 static void	sort_4(t_list **stack_a, t_list **stack_b)
 {
